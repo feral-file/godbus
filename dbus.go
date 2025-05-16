406ba55a93116499ac90514402c2bb90de93cf69
Hiếu Phạm <hieupham@bitmark.com>
2025-05-16 14:43:11 +0700

    add dbus wrapper that support signal only

---
+++ dbus.go
@@ -0,0 +1,310 @@
+package godbus
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
+	"github.com/godbus/dbus/v5"
+	"go.uber.org/zap"
+)
+
+type Member string
+
+const (
+	SYSTEM_NAME_ACQUIRED_MEMBER Member    = "NameAcquired"
+	SYSTEM_DBUS_INTERFACE       Interface = "org.freedesktop.DBus"
+	SYSTEM_DBUS_PATH            Path      = "/org/freedesktop/DBus"
+)
+
+func (e Member) String() string {
+	return string(e)
+}
+
+func (e Member) ACK() Member {
+	return Member(fmt.Sprintf("%s_ack", e))
+}
+
+func (e Member) IsACK() bool {
+	return strings.HasSuffix(string(e), "_ack")
+}
+
+type Path dbus.ObjectPath
+
+func (p Path) String() string {
+	return string(p)
+}
+
+type Interface string
+
+func (i Interface) String() string {
+	return string(i)
+}
+
+type DBusPayload struct {
+	Interface Interface
+	Path      Path
+	Member    Member
+	Body      []any
+}
+
+func (p DBusPayload) Name() string {
+	return fmt.Sprintf("%s.%s", p.Interface, p.Member)
+}
+
+func (p DBusPayload) IsSystemNameAcquired() bool {
+	return p.Interface == SYSTEM_DBUS_INTERFACE &&
+		p.Path == SYSTEM_DBUS_PATH &&
+		p.Member == SYSTEM_NAME_ACQUIRED_MEMBER
+}
+
+type BusSignalHandler func(
+	ctx context.Context,
+	payload DBusPayload) ([]any, error)
+
+type DBusClient struct {
+	sync.Mutex
+
+	ctx               context.Context
+	conn              *dbus.Conn
+	sigChan           chan *dbus.Signal
+	doneChan          chan struct{}
+	logger            *zap.Logger
+	busSignalHandlers []BusSignalHandler
+	senderID          *string
+	matchOptions      []dbus.MatchOption
+}
+
+func NewDBusClient(ctx context.Context, logger *zap.Logger, matchOptions ...dbus.MatchOption) *DBusClient {
+	return &DBusClient{
+		ctx:               ctx,
+		sigChan:           make(chan *dbus.Signal, 10),
+		doneChan:          make(chan struct{}),
+		logger:            logger,
+		busSignalHandlers: []BusSignalHandler{},
+		matchOptions:      matchOptions,
+	}
+}
+
+func (c *DBusClient) Start() error {
+	c.logger.Info("Starting DBusClient")
+	conn, err := dbus.SessionBus()
+	if err != nil {
+		return err
+	}
+	c.conn = conn
+	conn.Signal(c.sigChan)
+
+	err = conn.AddMatchSignalContext(c.ctx, c.matchOptions...)
+	if err != nil {
+		return err
+	}
+
+	c.background()
+
+	return nil
+}
+
+func (c *DBusClient) background() {
+	go func() {
+		c.logger.Info("DBusClient background goroutine started")
+		for {
+			select {
+			case <-c.ctx.Done():
+				c.logger.Info("Context cancelled, stopping DBusClient")
+				c.Stop()
+				return
+			case <-c.doneChan:
+				c.logger.Info("DBusClient stopped")
+				return
+			case sig := <-c.sigChan:
+				if sig == nil {
+					c.logger.Warn("Received nil signal")
+					continue
+				}
+
+				c.logger.Info("Received DBus signal", zap.Any("signal", sig))
+				if err := c.handleSignalRecv(sig); err != nil {
+					c.logger.Error("Failed to handle signal", zap.Error(err))
+				}
+			}
+		}
+	}()
+}
+
+func (c *DBusClient) OnBusSignal(f BusSignalHandler) {
+	c.Lock()
+	defer c.Unlock()
+	c.busSignalHandlers = append(c.busSignalHandlers, f)
+}
+
+func (c *DBusClient) RemoveBusSignal(f BusSignalHandler) {
+	c.Lock()
+	defer c.Unlock()
+
+	for i, handler := range c.busSignalHandlers {
+		if fmt.Sprintf("%p", handler) == fmt.Sprintf("%p", f) {
+			c.busSignalHandlers = append(c.busSignalHandlers[:i], c.busSignalHandlers[i+1:]...)
+			break
+		}
+	}
+}
+
+// handleSignalRecv handles a received signal that's not an ACK
+func (c *DBusClient) handleSignalRecv(sig *dbus.Signal) error {
+	if c.senderID != nil && *c.senderID == sig.Sender {
+		c.logger.Info("Skip self-generated signal", zap.String("name", sig.Name), zap.String("path", string(sig.Path)), zap.String("member", sig.Name))
+		return nil
+	}
+
+	i := strings.LastIndex(sig.Name, ".")
+	if i == -1 {
+		return fmt.Errorf("invalid signal name: %s", sig.Name)
+	}
+	iface := Interface(sig.Name[:i])
+	member := Member(sig.Name[i+1:])
+	path := Path(sig.Path)
+
+	payload := DBusPayload{
+		Interface: iface,
+		Path:      path,
+		Member:    member,
+		Body:      sig.Body,
+	}
+
+	// Skip system name acquired signals
+	if payload.IsSystemNameAcquired() {
+		if len(sig.Body) == 0 {
+			return fmt.Errorf("system name acquired signal has no body")
+		}
+		senderID, ok := sig.Body[0].(string)
+		if !ok {
+			return fmt.Errorf("system name acquired signal body doesn't contain a sender ID string")
+		}
+		c.senderID = &senderID
+		return nil
+	}
+
+	// Send ACK
+	if !member.IsACK() {
+		err := c.SendACK(payload)
+		if err != nil {
+			c.logger.Warn("Failed to send ACK", zap.String("interface", iface.String()), zap.String("path", path.String()), zap.String("member", member.String()), zap.Error(err))
+		}
+	}
+
+	for _, handler := range c.busSignalHandlers {
+		p := payload
+		h := handler
+
+		// Run the handler in a separate goroutine to avoid blocking the main thread
+		go func(ctx context.Context, payload DBusPayload, handler BusSignalHandler) error {
+			// Handle signal
+			_, err := handler(ctx, payload)
+			if err != nil {
+				c.logger.Warn("Failed to handle signal", zap.String("interface", iface.String()), zap.String("path", path.String()), zap.String("member", member.String()), zap.Error(err))
+				return err
+			}
+
+			return nil
+		}(c.ctx, p, h)
+	}
+
+	return nil
+}
+
+func (c *DBusClient) SendACK(payload DBusPayload) error {
+	return c.Send(DBusPayload{
+		Interface: payload.Interface,
+		Path:      payload.Path,
+		Member:    payload.Member.ACK(),
+		Body:      nil,
+	})
+}
+
+// RetryableSend retries sending a signal until an ACK is received
+// This function will be blocked until an ACK is received or the context is cancelled or the backoff timer expires
+// So it should be called in a separate goroutine unless you want to block the main thread
+func (c *DBusClient) RetryableSend(ctx context.Context, payload DBusPayload) error {
+	bo := backoff.NewExponentialBackOff()
+	bo.InitialInterval = 5 * time.Second
+	bo.MaxElapsedTime = 30 * time.Second
+
+	// Create a channel to receive ACK
+	ackChan := make(chan struct{})
+
+	// Create a temporary handler to listen for ACK
+	var handler BusSignalHandler
+	handler = func(ctx context.Context, p DBusPayload) ([]interface{}, error) {
+		// Check if this is the ACK for our signal
+		if p.Member == payload.Member.ACK() {
+			close(ackChan)
+			// Remove this temporary handler
+			c.RemoveBusSignal(handler)
+			return nil, nil
+		}
+		return nil, nil
+	}
+
+	// Add the temporary handler
+	c.OnBusSignal(handler)
+	defer c.RemoveBusSignal(handler)
+
+	// Retry ops
+	attempts := 0
+	ops := func() error {
+		attempts++
+		c.logger.Info(fmt.Sprintf("Sending signal with %d attempts", attempts), zap.String("interface", payload.Interface.String()), zap.String("path", payload.Path.String()), zap.String("member", payload.Member.String()), zap.Any("body", payload.Body))
+
+		// Send the signal
+		if err := c.Send(payload); err != nil {
+			c.logger.Error("Failed to send signal", zap.Error(err))
+			return err
+		}
+
+		// Wait for ACK with timeout
+		select {
+		case <-ackChan:
+			return nil // ACK received, success
+		case <-ctx.Done():
+			return backoff.Permanent(ctx.Err()) // Context cancelled
+		case <-time.After(bo.NextBackOff()):
+			return fmt.Errorf("timeout waiting for ACK") // Timeout, will retry
+		}
+	}
+
+	return backoff.Retry(ops, bo)
+}
+
+func (c *DBusClient) Send(payload DBusPayload) error {
+	c.Lock()
+	defer c.Unlock()
+
+	c.logger.Info("Sending signal", zap.String("interface", payload.Interface.String()), zap.String("path", payload.Path.String()), zap.String("member", payload.Member.String()), zap.Any("body", payload.Body))
+	return c.conn.Emit(dbus.ObjectPath(payload.Path), payload.Name(), payload.Body...)
+}
+
+func (c *DBusClient) Stop() error {
+	c.Lock()
+	defer c.Unlock()
+
+	c.logger.Info("Stopping DBusClient")
+
+	select {
+	case <-c.doneChan:
+		// Already closed
+	default:
+		close(c.doneChan)
+	}
+
+	if c.conn != nil {
+		c.conn.Close()
+		c.conn = nil
+		c.logger.Info("DBusClient connection closed")
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/feral-file/godbus
+
+go 1.23.5
+
+require (
+	github.com/cenkalti/backoff/v4 v4.3.0
+	github.com/godbus/dbus/v5 v5.1.0
+	go.uber.org/zap v1.27.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
